259dcf92b125c1829d1de2ba29867396349cd2f4
Shashwat Singh <s10singh97@yahoo.in>
2019-04-21 00:11:51 +0530

    reader writer problem

--- 15Apr/1.cpp
+++ 15Apr/1.cpp
@@ -2,20 +2,17 @@
 
 #include<iostream>
 #include<pthread.h>
-#include<string.h>
 #include<semaphore.h>
 #include<unistd.h>
 using namespace std;
 
 sem_t wrt, mutex;
 int readcount = 0;
-string str = "HelloWorld";
 
 void *writer(void *arg)
 {
     sem_wait(&wrt);
-    cout<<"write"<<endl;
-    cout<<str<<" "<<"\n";
+    cout<<"Write\n";
     sem_post(&wrt);
 }
 
@@ -25,10 +22,8 @@ void *reader(void *arg)
     readcount++;
     if(readcount == 1)
         sem_wait(&wrt);
-    cout<<"Read"<<endl;
-    cout<<"Reader No: "<<readcount<<"\n";
-    
     sem_post(&mutex);
+    cout<<"Read\n";
     sem_wait(&mutex);
     readcount--;
     if(readcount == 0)
@@ -42,23 +37,17 @@ int main(int argc, char const *argv[])
     sem_init(&wrt, 0, 1);
     sem_init(&mutex, 0, 1);
     for(int i = 0; i < 3; i++)
-    {
         pthread_create(&a[i], NULL, reader, NULL);
-        cout<<i<<endl;
-    }
     for(int i = 3; i < 5; i++)
-    {
         pthread_create(&a[i], NULL, writer, NULL);
-        cout<<i<<endl;
-    }
+    sleep(2);
     pthread_join(a[0], NULL);
     pthread_join(a[1], NULL);
     pthread_join(a[2], NULL);
     sleep(2);
-    /*pthread_join(a[0], NULL);
-    pthread_join(a[1], NULL);
+    pthread_join(a[0], NULL);
     pthread_join(a[3], NULL);
-    sleep(1);*/
+    sleep(2);
     pthread_join(a[3], NULL);
     pthread_join(a[4], NULL);
     sem_destroy(&wrt);
